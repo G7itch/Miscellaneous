da082a05ea517ab88b7b48e648986e72ec58658a
Tommykins27 <radioactivehuman64@gmail.com>
2023-03-19 14:29:27 +0000

    Finished Stalin sort
    Stalin sort now works

--- Sorting alogirthms/stalin sort/StalinSort.py
+++ Sorting alogirthms/stalin sort/StalinSort.py
@@ -2,33 +2,38 @@ import random
 import os
 
 file = open("list.txt","rt")
-list = file.readlines()
+lista = file.readlines()
 file.close()
-list = " ".join(map(str,list))
-list = list.split(",")
+lista = " ".join(map(str,lista))
+lista = lista.split(",")
 
-for element in list:
+for element in lista:
   element = int(element)
 
 listsorted = False
 choices = [True,False]
 for i in range(8):
   file = open(f"comrade{i}.txt","w")
-  file.write(random.choice(choices))
+  file.write(str(random.choice(choices)))
   file.close()
 
+numagree = 0
 while not(listsorted):
-  directory = 'stalin sort'
+  directory = '.'
   for filename in os.listdir(directory):
-      if os.path.isfile(f):
+      if os.path.isfile(filename):
         if filename == "list.txt" or filename == "StalinSort.py":
           pass
         else:
           file = open(filename,"rt")
           word = "".join(file.readlines())
-          if word == False:
+          if word == "False":
             os.remove(filename)
-
+          else:
+            numagree += 1
+  if numagree == len(os.listdir(directory))-2:
+    print(lista)
+    listsorted = True
     ##CHECK IF THEY ARE ALL TRUE, IF THEY ARE CHANGE THE FLAG
         
   
