d94f9bb8cfd1d11145b4c164edabb6766c32cf9b
Tommykins27 <radioactivehuman64@gmail.com>
2023-03-20 14:23:02 +0000

    Update SolarBitFlipSort.py
    Fixed code and added print

--- Sorting alogirthms/SolarBitFlipSort.py
+++ Sorting alogirthms/SolarBitFlipSort.py
@@ -1,18 +1,19 @@
 import random
 
 file = open("list.txt","rt")
-list = file.readlines()
+list1 = file.readlines()
 file.close()
-list = " ".join(map(str,list))
-list = list.split(",")
+list1 = " ".join(map(str,list1))
+list1 = list1.split(",")
 
-for element in list:
+for element in list1:
   element = int(element)
 
-while list != sorted(list):
+while list1 != sorted(list1):
   chance = random.randint(0,100000000000)
   if chance <= 10:
-    b = random.randint(0,len(list))
-    c = random.randint(0,len(list))
-    list[b],list[c] = list[c],list[b]
+    b = random.randint(0,len(list1))
+    c = random.randint(0,len(list1))
+    list1[b],list1[c] = list1[c],list1[b]
 
+print(list1)
